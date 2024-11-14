1f014f234d6059f03c2c2c42e630e03a899416a6
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2024-11-14 11:26:18 +0200

    Created a couresel

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -43,9 +43,11 @@ class _HomeState extends State<Home> {
         child: Column(
           children: [
             Container(
+              height: 70,
               // reason for ListView.builder: show all list of categories
               child: ListView.builder(
                   shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
                   itemCount: categories.length,
                   itemBuilder: (context, index) {
                     return CategoryTile(
@@ -69,12 +71,33 @@ class CategoryTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.only(right: 16),
       child: Stack(
         children: [
-          Image.asset(
-            image,
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Image.asset(
+              image,
+              width: 120,
+              height: 60,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
             width: 120,
             height: 60,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(6),
+              color: Colors.black26,
+            ),
+            child: Center(
+              child: Text(
+                categoryName,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
           ),
         ],
       ),
